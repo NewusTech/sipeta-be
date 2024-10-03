4d3ba5a9c71041446679623f18dfc2ad142146f0
Newus Technologi <newustechnology@gmail.com1>
2024-10-03 16:13:44 +0700

    update

--- routes/main/datatoponim.route.js
+++ routes/main/datatoponim.route.js
@@ -31,5 +31,6 @@ route.get('/datatoponim/shp', [mid.checkRoles()], datatoponimController.shp);
 route.post('/datatoponim/import-json', [mid.checkRoles()], upload.single('file'), importtoponimController.importJson);
 route.post('/datatoponim/import-excel', [mid.checkRoles()], upload.single('file'), importtoponimController.importExcel);
 route.post('/datatoponim/import-csv', [mid.checkRoles()], upload.single('file'), importtoponimController.importCsv);
+route.post('/datatoponim/import-shp', [mid.checkRoles()], upload.single('file'), importtoponimController.importShp);
 
 module.exports = route;
